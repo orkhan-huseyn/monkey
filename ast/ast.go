35372352c0af8d72711197be4787d098b9f803b2
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2020-05-10 14:25:44 +0400

    add: return statement to ast

--- ast/ast.go
+++ ast/ast.go
@@ -28,6 +28,14 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) statementNode()       {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
@@ -37,10 +45,10 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
-type Identifier struct {
-	Token token.Token
-	Value string
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
 }
 
-func (i *Identifier) statementNode()       {}
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
