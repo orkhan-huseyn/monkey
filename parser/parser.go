36ef1f700b518eb3cdeb70bcda2d4a418f6d0bda
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2020-05-10 14:32:15 +0400

    test and parse return statement

--- parser/parser.go
+++ parser/parser.go
@@ -57,6 +57,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -81,6 +83,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		p.nextToken()
 	}
 
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	//TODO: We're skipping the expression
+	// until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
 	return stmt
 }
 
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -42,6 +42,35 @@ func TestLetStatements(t *testing.T) {
 	}
 }
 
+func TestReturnStatements(t *testing.T) {
+	input := `
+		return 5;
+		return 10;
+		return 993322;
+	`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 elements, god %d\n", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt is not *ast.ReturnStatement. Got %T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("retursStmt.TokenLiteral is not 'return'. Got %q", returnStmt.TokenLiteral())
+		}
+	}
+
+}
+
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 
 	if s.TokenLiteral() != "let" {
