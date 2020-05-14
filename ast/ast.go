053b937b972e4581c64f8cdef2ac9af7adf4546c
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2020-05-14 12:56:14 +0400

    add: infix expressions and test case

--- ast/ast.go
+++ ast/ast.go
@@ -5,31 +5,38 @@ import (
 	"monkey/token"
 )
 
+// Node is an AST node interface
 type Node interface {
 	TokenLiteral() string
 	String() string
 }
 
+// Statement is an interface for
+// AST nodes denoting statement
 type Statement interface {
 	Node
 	statementNode()
 }
 
+// Expression is an interface to
+// denote expression on AST
 type Expression interface {
 	Node
 	expressionNode()
 }
 
+// Program is root Node of AST
 type Program struct {
 	Statements []Statement
 }
 
+// TokenLiteral is just a dummy method
+// to denote that Program is an AST Node
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
-	} else {
-		return ""
 	}
+	return ""
 }
 
 func (p *Program) String() string {
@@ -40,22 +47,32 @@ func (p *Program) String() string {
 	return out.String()
 }
 
+// Identifier denotes an
+// identifier expression
 type Identifier struct {
 	Token token.Token
 	Value string
 }
 
-func (i *Identifier) expressionNode()      {}
+func (i *Identifier) expressionNode() {}
+
+// TokenLiteral is just a dummy method
+// to denote that Identifier is an AST Node
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
 
+// LetStatement represents
+// a let statement
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral is just a dummy method
+// to denote that LetStatement is an AST Node
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 func (ls *LetStatement) String() string {
 	var out bytes.Buffer
@@ -73,12 +90,17 @@ func (ls *LetStatement) String() string {
 	return out.String()
 }
 
+// ReturnStatement represents
+// a return statement
 type ReturnStatement struct {
 	Token       token.Token
 	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) statementNode() {}
+
+// TokenLiteral is just a dummy method
+// to denote that ReturnStatement is an AST Node
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 func (rs *ReturnStatement) String() string {
 	var out bytes.Buffer
@@ -94,12 +116,17 @@ func (rs *ReturnStatement) String() string {
 	return out.String()
 }
 
+// ExpressionStatement represents
+// an expression statement
 type ExpressionStatement struct {
 	Token      token.Token
 	Expression Expression
 }
 
-func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) statementNode() {}
+
+// TokenLiteral is just a dummy method
+// to denote that ExpressionStatement is an AST Node
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 func (es *ExpressionStatement) String() string {
 	if es.Expression != nil {
@@ -108,22 +135,32 @@ func (es *ExpressionStatement) String() string {
 	return ""
 }
 
+// IntegerLiteral represents
+// an integer literal expression
 type IntegerLiteral struct {
 	Token token.Token
 	Value int64
 }
 
-func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) expressionNode() {}
+
+// TokenLiteral is just a dummy method
+// to denote that IntegerLiteral is an AST Node
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
 
+// PrefixExpression represents
+// a prefix expression. e.g. !10 or ++10
 type PrefixExpression struct {
 	Token    token.Token
 	Operator string
 	Right    Expression
 }
 
-func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) expressionNode() {}
+
+// TokenLiteral is just a dummy method
+// to denote that PrefixExpression is an AST Node
 func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
 func (pe *PrefixExpression) String() string {
 	var out bytes.Buffer
@@ -131,6 +168,32 @@ func (pe *PrefixExpression) String() string {
 	out.WriteString("(")
 	out.WriteString(pe.Operator)
 	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+// InfixExpression represents
+// a prefix expression. e.g. 5 + 5 or 20 / 10
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode() {}
+
+// TokenLiteral is just a dummy method
+// to denote that InfixExpression is an AST Node
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
 	out.WriteString(")")
 
 	return out.String()
